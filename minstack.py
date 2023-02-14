f694eb706fe72b2cfd9c07442f3430ff6ca971cf
Johnny <125358378+johnwickermann33@users.noreply.github.com>
2023-02-14 11:24:12 +0100

    Add fast MinStack implementation

---
+++ minstack.py
@@ -0,0 +1,23 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        if not self.min_stack or x <= self.min_stack[-1]:
+            self.min_stack.append(x)
+
+    def pop(self) -> None:
+        if self.stack:
+            x = self.stack.pop()
+            if x == self.min_stack[-1]:
+                self.min_stack.pop()
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+
+    def getMin(self) -> int:
+        if self.min_stack:
+            return self.min_stack[-1]
