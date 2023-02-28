ea0254a03b8a00f558329ee3c2efd939886fde6a
Johnny <125358378+johnwickermann33@users.noreply.github.com>
2023-02-28 15:30:33 +0100

    Create migration.java

---
+++ migration.java
@@ -0,0 +1,12 @@
+package migrations.flyway;
+
+import common.Migration;
+import common.flyway.BaseFlywayMigration;
+
+@SuppressWarnings({"checkstyle:TypeName", "java:S101"})
+public class R__Create_Consumer_Topic extends BaseFlywayMigration {
+
+    public R__Create_Consumer_Topic(final Migration migration) {
+        super(migration);
+    }
+}
